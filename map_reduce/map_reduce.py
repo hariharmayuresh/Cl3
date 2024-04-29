ef2fbe70ee149b447bf34d06c5dbcf92f9f7d46f
hariharmayuresh <103446113+hariharmayuresh@users.noreply.github.com>
2024-04-29 09:46:03 +0530

    Update map_reduce.py

--- map_reduce/map_reduce.py
+++ map_reduce/map_reduce.py
@@ -1,3 +1,6 @@
+'''Design a distributed application using MapReduce under Hadoop for: a) Character 
+counting in a given text file. b) Counting no. of occurrences of every word in a given text 
+file '''
 
 from mrjob. job import MRJob
 import re
